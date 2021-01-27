2a616732a2d69a16147ddde1dadb8c44717c261a
Ermiyas Solomon <ermiyasst@gmail.com>
2021-01-27 15:31:10 +0300

    home page comitting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
-];
+// const routes: Routes = [
+//   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+//   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+// ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
