a5b72ef63aae033ec7554d533736bc128cf65fcf
Ermiyas Solomon <ermiyasst@gmail.com>
2021-02-13 13:08:00 +0300

    6/afternoon changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBv3W06XroMN1n544WazjOFDf0eGcfjp0E",
+    authDomain: "teguaz-web-app.firebaseapp.com",
+    projectId: "teguaz-web-app",
+    storageBucket: "teguaz-web-app.appspot.com",
+    messagingSenderId: "252301881886",
+    appId: "1:252301881886:web:187f130f8fffa883f1340f",
+    measurementId: "G-GNDCHKQ1PB"
+  }
 };
 
 /*
