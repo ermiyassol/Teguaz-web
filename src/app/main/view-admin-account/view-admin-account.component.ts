248e0529beb8aa092b491291784d52364fa34290
Ermiyas Solomon <ermiyasst@gmail.com>
2021-02-16 16:51:45 +0300

    10/06 afternoon commited

--- src/app/main/view-admin-account/view-admin-account.component.ts
+++ src/app/main/view-admin-account/view-admin-account.component.ts
@@ -2,26 +2,39 @@ import { AdminModel } from './../../models/Admin.model';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AdminAccountService } from 'src/app/services/admin-account.service';
 import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-admin-account',
   templateUrl: './view-admin-account.component.html',
-  styleUrls: ['./view-admin-account.component.css']
+  styleUrls: ['./view-admin-account.component.css'],
 })
 export class ViewAdminAccountComponent implements OnInit, OnDestroy {
   admins: AdminModel[] = [];
   subscription: Subscription;
+  isLoading = false;
 
-  constructor(private adminService: AdminAccountService) { }
+  openDrawer(i: number) {
+    this.routes.navigate([i], { relativeTo: this.route });
+    // console.log(i);
+  }
+
+  constructor(
+    private adminService: AdminAccountService,
+    private routes: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.subscription = this.adminService.adminList.subscribe(response => {
+    this.isLoading = true;
+    this.adminService.setAdmins();
+    this.subscription = this.adminService.adminList.subscribe((response) => {
       this.admins = response;
-    })
+      this.isLoading = false;
+    });
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
-
 }
